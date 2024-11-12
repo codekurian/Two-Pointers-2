355b1fe5c52d55120c28bd8ae812b46292717e45
kurian <openfeather@hotmail.com>
2024-11-12 09:54:11 -0500

    Added Problem1

--- Problem1.java
+++ Problem1.java
@@ -5,3 +5,41 @@
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+class Problem1 {
+    //fast slow pointer: Sol1
+    //Sol2 ; bucket sort... count the values and create an array
+
+
+    public int removeDuplicates(int[] nums) {
+        return fastSlowPtr(nums);
+    }
+
+    //TC: O(N)
+    //SC :O(1)
+    public int fastSlowPtr(int[] nums){
+        int slow = 0;//points to number that has to be swapped
+        int fast = 0; //for tracking change of the numbers
+        int k = 2;
+        int count =0;
+        while(fast<nums.length){
+            if(fast>0 && nums[fast] == nums[fast -1]){
+                //this means that the element is same and not changed
+                count++;
+
+            }else{
+                //number changed
+                count =1;
+
+            }
+            if(count<=k){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+
+
+}
